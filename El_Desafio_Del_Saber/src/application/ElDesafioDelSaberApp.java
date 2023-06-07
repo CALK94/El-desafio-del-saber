36872fa4c278e83c3c77c944e4b9b251bb5926b2
carlo <carlo@LAPTOP-35C0QDQ1>
2023-06-07 15:45:02 -0500

    Adicion de comentarios

--- El_Desafio_Del_Saber/src/application/ElDesafioDelSaberApp.java
+++ El_Desafio_Del_Saber/src/application/ElDesafioDelSaberApp.java
@@ -6,15 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class ElDesafioDelSaberApp extends Application {
+public class ElDesafioDelSaberApp extends Application { //JavaFX
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-    	FXMLLoader loader = new FXMLLoader(getClass().getResource("ElDesafioVentana.fxml"));
-    	Parent root = loader.load();
-        primaryStage.setTitle("El desafio del saber");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("ElDesafioVentana.fxml"));	//se utiliza para cargar el archivo FXML que define la apariencia de la interfaz grafica
+    	Parent root = loader.load();	//se carga el archivo y se obtiene el nodo raiz
+        primaryStage.setTitle("El desafio del saber");	//titulo de la ventana principal
+        primaryStage.setScene(new Scene(root));	//se crena crea una nueva escena con el nodo de raiz
+        primaryStage.show();	//esto hace que la vetana se haga visible para el usuario
     }
 
     public static void main(String[] args) {
--- El_Desafio_Del_Saber/src/application/ElDesafioDelSaberController.java
+++ El_Desafio_Del_Saber/src/application/ElDesafioDelSaberController.java
@@ -9,85 +9,84 @@ import javafx.scene.control.ToggleGroup;
 
 public class ElDesafioDelSaberController {
 
-    @FXML
+    @FXML //anotacion definida para identificar y bautizar elemento en SceneBuilder
     private Label questionLabel;
 
-    @FXML
+    @FXML //anotacion definida para identificar y bautizar elemento en SceneBuilder
     private RadioButton option1RadioButton;
 
-    @FXML
+    @FXML //anotacion definida para identificar y bautizar elemento en SceneBuilder
     private RadioButton option2RadioButton;
 
-    @FXML
+    @FXML //anotacion definida para identificar y bautizar elemento en SceneBuilder
     private RadioButton option3RadioButton;
 
-    @FXML
+    @FXML //anotacion definida para identificar y bautizar elemento en SceneBuilder
     private Button submitButton;
 
-    @FXML
+    @FXML //anotacion definida para identificar y bautizar elemento en SceneBuilder
     private Label resultLabel;
 
-    private ToggleGroup optionsGroup;
+    private ToggleGroup optionsGroup;		//Variable para la agrupacion de los Radio Button
 
-    private int currentQuestionIndex = 0;
-    private int score = 0;
+    private int currentQuestionIndex = 0;	//Variable para realizar seguimiento del indice de la pregunta actual
+    private int score = 0;					//Variable para realizar seguimiento del score actual
 
-    private String[] questions = {
-            "¿Cuál es la capital de Colombia?",
-            "¿Cuál es el río más largo del mundo?",
-            "¿Cuál es el año de la independencia de Estados Unidos?"
+    private String[] questions = {			//matriz 1 almacena las preguntas
+            "¿Cuál es la ciudad Colombiana conocida como \"La Ciudad de la Eterna Primavera\"?",
+            "¿Cuál es el principal deporte de Colombia?",
+            "¿Quién fue el primer presidente de Colombia?"
     };
 
-    private String[][] options = {
+    private String[][] options = {			//matriz 2 almacena las respuestas
             {"Bogotá", "Medellín", "Cali"},
-            {"Amazonas", "Nilo", "Yangtsé"},
-            {"1776", "1789", "1812"}
+            {"Patinaje", "Tejo", "Futbol"},
+            {"Simón Bolívar", "Francisco de Paula Santander", "José Miguel Pey de Andrade"}
     };
 
-    private int[] correctAnswers = {0, 1, 0};
+    private int[] correctAnswers = {1, 2, 0};		//array almacena el indice de la opcion de la respuesta correcta para cada pregunta
 
     @FXML
-    private void initialize() {
+    private void initialize() {		//metodo se llama cuando se carga la interfaz grafica y se configura anotacion definidas
     	questionLabel.setText(questions[0]);
     	option1RadioButton.setText(options[0][0]);
     	option2RadioButton.setText(options[0][1]);
     	option3RadioButton.setText(options[0][2]);
     	submitButton.setText("Enviar");
     	resultLabel.setText("");
-        optionsGroup = new ToggleGroup();
-        option1RadioButton.setToggleGroup(optionsGroup);
+        optionsGroup = new ToggleGroup();	//crea el objeto de la clase y luego se configura en los botones 1,2 y 3.
+        option1RadioButton.setToggleGroup(optionsGroup); //es utilizado para agrupar los botones y asi solo se pueda seleccionar uno de ellos.
         option2RadioButton.setToggleGroup(optionsGroup);
         option3RadioButton.setToggleGroup(optionsGroup);
     }
 
     @FXML
-    private void submitButtonClicked(ActionEvent event) {
-        RadioButton selectedRadioButton = (RadioButton) optionsGroup.getSelectedToggle();
+    private void submitButtonClicked(ActionEvent event) {										//metodo se llama al darle click al boton "Enviar"
+        RadioButton selectedRadioButton = (RadioButton) optionsGroup.getSelectedToggle();		
         if (selectedRadioButton != null) {
-            int selectedAnswer = optionsGroup.getToggles().indexOf(selectedRadioButton);
-            if (selectedAnswer == correctAnswers[currentQuestionIndex]) {
-                score++;
-                resultLabel.setText("¡Respuesta correcta!");
-            } else {
-                resultLabel.setText("Respuesta incorrecta. La respuesta correcta era: " +
-                        options[currentQuestionIndex][correctAnswers[currentQuestionIndex]]);
+            int selectedAnswer = optionsGroup.getToggles().indexOf(selectedRadioButton);		
+            if (selectedAnswer == correctAnswers[currentQuestionIndex]) {						// Si la respuesta es correcta 
+                score++;																		// sumar al score
+                resultLabel.setText("¡Respuesta correcta!");									// y arrojar mensaje
+            } else {																			// Si no... 
+                resultLabel.setText("Respuesta incorrecta. La respuesta correcta era: " +		//arrojar mensaje 
+                        options[currentQuestionIndex][correctAnswers[currentQuestionIndex]]); 	// de la respuesta correcta
             }
 
-            if (currentQuestionIndex < questions.length - 1) {
-                currentQuestionIndex++;
-                showQuestion();
-            } else { 
+            if (currentQuestionIndex < questions.length - 1) {									// Si aun no se han terminado las preguntas
+                currentQuestionIndex++;															// sumar al contador actual de preguntas
+                showQuestion();                                                                 // mostrar la pregunta segun el contador
+            } else { 																			// si no... (si ya se terminaron las preguntas)
             	
-            	if(selectedAnswer == correctAnswers[currentQuestionIndex]) {
-            		resultLabel.setText("¡Respuesta correcta!" + "\n" + "Juego terminado. Puntuación: " + score + "/" + questions.length );
-            	} else {
-            		resultLabel.setText("Respuesta incorrecta. La respuesta correcta era: " +
+            	if(selectedAnswer == correctAnswers[currentQuestionIndex]) {					// si la respuesta es correcta
+            		resultLabel.setText("¡Respuesta correcta!" + "\n" + "Juego terminado. Puntuación: " + score + "/" + questions.length );	//mostrar este mensaje
+            	} else {																		//si no
+            		resultLabel.setText("Respuesta incorrecta. La respuesta correcta era: " +	//mostrar este mensaje
                             options[currentQuestionIndex][correctAnswers[currentQuestionIndex]] + "\n" +
                             "Juego terminado. Puntuación: " + score + "/" + questions.length);
-            	}
+            	}         	
             	
-            	
-                //resultLabel.setText(  + "\n" + "Juego terminado. Puntuación: " + score + "/" + questions.length );
+               
                 submitButton.setDisable(true);
             }
             
@@ -96,12 +95,12 @@ public class ElDesafioDelSaberController {
         }
     }
 
-    private void showQuestion() {
-        questionLabel.setText(questions[currentQuestionIndex]);
-        option1RadioButton.setText(options[currentQuestionIndex][0]);
+    private void showQuestion() {	//este metodo actualiza los elementos de la interfaz grafica SceneBuilder
+        questionLabel.setText(questions[currentQuestionIndex]);	//establece el texto en el label con la pregunta actual que obtiene del array
+        option1RadioButton.setText(options[currentQuestionIndex][0]); //establece el texto en el Radio Button con la opcion de respuesta que obtiene de la matriz
         option2RadioButton.setText(options[currentQuestionIndex][1]);
         option3RadioButton.setText(options[currentQuestionIndex][2]);
-        optionsGroup.selectToggle(null);
+        optionsGroup.selectToggle(null); //si seleccion un boton y luego otro, deselecciona el anterior
        
     }
 }
