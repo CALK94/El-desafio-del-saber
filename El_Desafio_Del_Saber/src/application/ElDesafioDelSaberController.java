574559ae0929069b49d32d39153bb596b3975fc0
carlo <carlo@LAPTOP-35C0QDQ1>
2023-06-12 18:28:52 -0500

    Actualizacion comentarios y nombres variables-metodos.

--- El_Desafio_Del_Saber/src/application/ElDesafioDelSaberController.java
+++ El_Desafio_Del_Saber/src/application/ElDesafioDelSaberController.java
@@ -10,84 +10,88 @@ import javafx.scene.control.ToggleGroup;
 public class ElDesafioDelSaberController {
 
     @FXML //anotacion definida para identificar y bautizar elemento en SceneBuilder
-    private Label questionLabel;
+    private Label preguntaLabel;
 
     @FXML //anotacion definida para identificar y bautizar elemento en SceneBuilder
-    private RadioButton option1RadioButton;
+    private RadioButton respuestaOpcion1RadioButton;
 
     @FXML //anotacion definida para identificar y bautizar elemento en SceneBuilder
-    private RadioButton option2RadioButton;
+    private RadioButton respuestaOpcion2RadioButton;
 
     @FXML //anotacion definida para identificar y bautizar elemento en SceneBuilder
-    private RadioButton option3RadioButton;
+    private RadioButton respuestaOpcion3RadioButton;
 
     @FXML //anotacion definida para identificar y bautizar elemento en SceneBuilder
-    private Button submitButton;
+    private Button enviarButton;
 
     @FXML //anotacion definida para identificar y bautizar elemento en SceneBuilder
-    private Label resultLabel;
+    private Label resultadoLabel;
 
-    private ToggleGroup optionsGroup;		//Variable para la agrupacion de los Radio Button
+    private ToggleGroup grupoRespuestas;		//Variable para la agrupacion de los Radio Button
 
-    private int currentQuestionIndex = 0;	//Variable para realizar seguimiento del indice de la pregunta actual
-    private int score = 0;					//Variable para realizar seguimiento del score actual
+    private int indicePreguntaActual = 0;		//Variable para realizar seguimiento del indice de la pregunta actual
+    private int puntaje = 0;					//Variable para realizar seguimiento del score actual
 
-    private String[] questions = {			//matriz 1 almacena las preguntas
+    private String[] preguntas = {			//matriz 1 almacena las preguntas
             "¿Cuál es la ciudad Colombiana conocida como \"La Ciudad de la Eterna Primavera\"?",
             "¿Cuál es el principal deporte de Colombia?",
-            "¿Quién fue el primer presidente de Colombia?"
+            "¿Quién fue el primer presidente de Colombia?",
+            "¿Cuantos departamentos tiene Colombia?"
+            
     };
 
-    private String[][] options = {			//matriz 2 almacena las respuestas
+    private String[][] opcionesRespuesta = {			//matriz 2 almacena las respuestas
             {"Bogotá", "Medellín", "Cali"},
             {"Patinaje", "Tejo", "Futbol"},
-            {"Simón Bolívar", "Francisco de Paula Santander", "José Miguel Pey de Andrade"}
+            {"Simón Bolívar", "Francisco de Paula Santander", "José Miguel Pey de Andrade"},
+            {"28", "32", "30"}
+            
     };
 
-    private int[] correctAnswers = {1, 2, 0};		//array almacena el indice de la opcion de la respuesta correcta para cada pregunta
+    private int[] respuestasCorrectas = {1, 2, 0, 1};		//array almacena el indice de la opcion de la respuesta correcta para cada pregunta
 
     @FXML
     private void initialize() {		//metodo se llama cuando se carga la interfaz grafica y se configura anotacion definidas
-    	questionLabel.setText(questions[0]);
-    	option1RadioButton.setText(options[0][0]);
-    	option2RadioButton.setText(options[0][1]);
-    	option3RadioButton.setText(options[0][2]);
-    	submitButton.setText("Enviar");
-    	resultLabel.setText("");
-        optionsGroup = new ToggleGroup();	//crea el objeto de la clase y luego se configura en los botones 1,2 y 3.
-        option1RadioButton.setToggleGroup(optionsGroup); //es utilizado para agrupar los botones y asi solo se pueda seleccionar uno de ellos.
-        option2RadioButton.setToggleGroup(optionsGroup);
-        option3RadioButton.setToggleGroup(optionsGroup);
+    	preguntaLabel.setText(preguntas[0]);
+    	respuestaOpcion1RadioButton.setText(opcionesRespuesta[0][0]);
+    	respuestaOpcion2RadioButton.setText(opcionesRespuesta[0][1]);
+    	respuestaOpcion3RadioButton.setText(opcionesRespuesta[0][2]);
+    	enviarButton.setText("Enviar");
+    	resultadoLabel.setText("");
+    	grupoRespuestas = new ToggleGroup();	//crea el objeto de la clase y luego se configura en los botones 1,2 y 3.
+        respuestaOpcion1RadioButton.setToggleGroup(grupoRespuestas); //es utilizado para agrupar los botones y asi solo se pueda seleccionar uno de ellos.
+        respuestaOpcion2RadioButton.setToggleGroup(grupoRespuestas);
+        respuestaOpcion3RadioButton.setToggleGroup(grupoRespuestas);
     }
 
     @FXML
-    private void submitButtonClicked(ActionEvent event) {										//metodo se llama al darle click al boton "Enviar"
-        RadioButton selectedRadioButton = (RadioButton) optionsGroup.getSelectedToggle();		
-        if (selectedRadioButton != null) {
-            int selectedAnswer = optionsGroup.getToggles().indexOf(selectedRadioButton);		
-            if (selectedAnswer == correctAnswers[currentQuestionIndex]) {						// Si la respuesta es correcta 
-                score++;																		// sumar al score
-                resultLabel.setText("¡Respuesta correcta!");									// y arrojar mensaje
-            } else {																			// Si no... 
-                resultLabel.setText("Respuesta incorrecta. La respuesta correcta era: " +		//arrojar mensaje 
-                        options[currentQuestionIndex][correctAnswers[currentQuestionIndex]]); 	// de la respuesta correcta
+    private void enviarButtonClicked(ActionEvent event) {												//metodo se llama al darle click al boton "Enviar"
+        RadioButton selectedRadioButton = (RadioButton) grupoRespuestas.getSelectedToggle();		
+        if (selectedRadioButton != null) {																// se asegura que se seleccion alguna opcion
+            int respuestaSeleccionada = grupoRespuestas.getToggles().indexOf(selectedRadioButton);		//se obtiene el indice de la respuesta seleccionada y se almacena en respuestaSeleccionada
+            if (respuestaSeleccionada == respuestasCorrectas[indicePreguntaActual]) {					// Si la respuesta es correcta 
+            	puntaje++;																				// sumar al puntaje
+                resultadoLabel.setText("¡Respuesta correcta!");											// y arrojar mensaje
+            } else {																					// Si no... 
+            	resultadoLabel.setText("Respuesta incorrecta. La respuesta correcta era: " +			//arrojar mensaje 
+            			opcionesRespuesta[indicePreguntaActual][respuestasCorrectas[indicePreguntaActual]]); 	// de la respuesta correcta
             }
 
-            if (currentQuestionIndex < questions.length - 1) {									// Si aun no se han terminado las preguntas
-                currentQuestionIndex++;															// sumar al contador actual de preguntas
-                showQuestion();                                                                 // mostrar la pregunta segun el contador
+            if (indicePreguntaActual < preguntas.length - 1) {									// Si aun no se han terminado las preguntas
+            	indicePreguntaActual++;															// sumar al contador indicePreguntaActual (de esta forma se van cambiando las preguntas)
+            	mostrarPregunta();                                                              // mostrar la pregunta segun el contador indicePreguntaActual
             } else { 																			// si no... (si ya se terminaron las preguntas)
             	
-            	if(selectedAnswer == correctAnswers[currentQuestionIndex]) {					// si la respuesta es correcta
-            		resultLabel.setText("¡Respuesta correcta!" + "\n" + "Juego terminado. Puntuación: " + score + "/" + questions.length );	//mostrar este mensaje
-            	} else {																		//si no
-            		resultLabel.setText("Respuesta incorrecta. La respuesta correcta era: " +	//mostrar este mensaje
-                            options[currentQuestionIndex][correctAnswers[currentQuestionIndex]] + "\n" +
-                            "Juego terminado. Puntuación: " + score + "/" + questions.length);
+            	if(respuestaSeleccionada == respuestasCorrectas[indicePreguntaActual]) {					// si la respuesta es correcta
+            		resultadoLabel.setText("¡Respuesta correcta!" + "\n" + "Juego terminado. Puntuación: " + puntaje + "/" + preguntas.length );	//mostrar este mensaje
+            	} else {																			//si no
+            		resultadoLabel.setText("Respuesta incorrecta. La respuesta correcta era: " +	//mostrar este mensaje
+            				opcionesRespuesta[indicePreguntaActual][respuestasCorrectas[indicePreguntaActual]] + "\n" +
+                            "Juego terminado. Puntuación: " + puntaje + "/" + preguntas.length);
             	}         	
             	
                
-                submitButton.setDisable(true);
+            	enviarButton.setDisable(true);
             }
             
 
@@ -95,12 +99,12 @@ public class ElDesafioDelSaberController {
         }
     }
 
-    private void showQuestion() {	//este metodo actualiza los elementos de la interfaz grafica SceneBuilder
-        questionLabel.setText(questions[currentQuestionIndex]);	//establece el texto en el label con la pregunta actual que obtiene del array
-        option1RadioButton.setText(options[currentQuestionIndex][0]); //establece el texto en el Radio Button con la opcion de respuesta que obtiene de la matriz
-        option2RadioButton.setText(options[currentQuestionIndex][1]);
-        option3RadioButton.setText(options[currentQuestionIndex][2]);
-        optionsGroup.selectToggle(null); //si seleccion un boton y luego otro, deselecciona el anterior
+    private void mostrarPregunta() {	//este metodo actualiza los elementos de la interfaz grafica SceneBuilder
+    	preguntaLabel.setText(preguntas[indicePreguntaActual]);	//establece el texto en el label con la pregunta actual que obtiene del array
+        respuestaOpcion1RadioButton.setText(opcionesRespuesta[indicePreguntaActual][0]); //establece el texto en el Radio Button con la opcion de respuesta que obtiene de la matriz
+        respuestaOpcion2RadioButton.setText(opcionesRespuesta[indicePreguntaActual][1]);
+        respuestaOpcion3RadioButton.setText(opcionesRespuesta[indicePreguntaActual][2]);
+        grupoRespuestas.selectToggle(null); //si selecciona un boton y luego otro, deselecciona el anterior
        
     }
 }
